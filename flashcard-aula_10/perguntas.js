31d6aa8454c170664d89e862b9a5ae02007dd3e7
willyan28 <140612303+willyan28@users.noreply.github.com>
2024-11-21 11:01:45 -0300

    Add files via upload

--- flashcard-aula_10/perguntas.js
+++ flashcard-aula_10/perguntas.js
@@ -1,23 +1,27 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'PERSONAGEM',
+    'quem e kratos?',
+    'Kratos, como deus da guerra, passou a invadir e destruir várias cidades com a ajuda do exército espartano, e partiu para Rodes em uma dessas viagens. Zeus, no entanto, traiu Kratos em segredo e transferiu alguns de seus poderes para o gigantesco Colosso de Rodes'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'SUA HISTÓRIA E SUA MALDIÇÃO',
+    'SUA HISTÓRIA',
+    'Após derrotar o exército do bárbaro, Kratos vagou por toda a Grécia a serviço de Ares matando soldados e inocentes sem pensar duas vezes. O olimpiano então, viu a chance de pregar uma peça no seu novo comandado. sua maldição A maldição só será quebrada quando Kratos se perdoar completamente por ter matado sua família na Grécia, e é um tipo especial de magia, se as pessoas devem ser acreditadas. Quanto às lâminas, elas são um objeto significativo ligado a Kratos. Não importa para onde ele vá, elas estarão com ele até que ele morra.',
+
+    'Possuído pela sua incontrolável fúria, ele não reconheceu sua própria família, que foi teletransportada pelo deus da guerra para dentro de um templo no qual foi ordenado a invadir e assassinar todos. Lyssandra e Caliope, infelizmente, perceram ali.',
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'OS DEUSES ',
+    'Quantos deuses kratos matou?',
+    'ele matou as 3 fúrias, Orkos, Perséfone, Ares, Tânatos, Prometeu, as 3 irmãs do destino, Atena, Poseidon, Hades, Hélios, Hércules, Hermes, Hefesto, Hera, Zeus, Cronos, Gaia, Perses, algum monstro egípcio, Magni, Baldur, Heimdall, Calíope... e muitos outros seres mitológicos'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'AS MORTES',
+    'Qual a morte mais brutal ',
+    'Em sua busca por vingança contra Zeus, Kratos acabou batalhando praticamente com todos os deuses do Olimpo. O deus da guerra enfrentou Helios, personificação do sol na mitologia grega. A morte do deus foi uma das mais brutais vistas na franquia, com Kratos arrancando a sua cabeça apenas com a força das mãos e Mais uma morte brutal para a conta de Kratos. e O deus da guerra não deu chances para Hermes, o mensageiro dos deuses. O espartano cortou as duas pernas do deus para conseguir as suas botas. “Pensei que os espartanos lutassem com honra”, disse antes de morrer..',
+
+    
 )
